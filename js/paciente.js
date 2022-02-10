3ee61b0827aea35d90c849dc1a39a661cd6259b4
Kitsunito <hinoken.htp@gmail.com>
2022-02-09 20:44:25 -0500

    Entrega "Interactuar con HTML"

--- js/paciente.js
+++ js/paciente.js
@@ -1,12 +1,10 @@
-/*Esta aplicación tiene como objetivo brindar la información correspondiente a una persona
+/*Esta aplicación tiene como objetivo brindar la información correspondiente a personas
 que haya resultado positiva para COVID, en función de los siguientes parámetros:
 - Si tiene esquema de vacunación primario completo (es decir más de una dosis si corresponde)
 - Si tuvo síntomas (es decir, que tuvo una FIS (fecha de inicio de síntomas))
 - Su FTM (fecha de toma de muestra)
-Si la persona ya pasó de su fecha de alta
-    debe informar la situación.
-si no
-    debe indicar la fecha probable de alta.*/
+En función de lo que el usuario decida, se deben pedir los datos del paciente 
+y se debe indicar la fecha probable de alta.*/
 
 class tipoVacuna{
     constructor(id){
@@ -156,16 +154,16 @@ class Paciente{
     
     calcularFechaDeCurva(){
         //validamos que tenga FIS
-        var diaCero = new Date("12/01/2019")
-        var inicio = new Date(this.fis)
+        var diaCero = new Date("12/01/2019");
+        var inicio = new Date(this.fis);
         if (inicio.getTime() === diaCero.getTime())
-            return this.ftm;
+            return new Date(this.ftm);
         else {
             //Si no, devolvemos la menor de las fechas entre la FTM y la FIS
             if (this.fis >= this.ftm)
-                return this.ftm;
+                return new Date(this.ftm);
             else
-                return this.fis;
+                return new Date(this.fis);
         }
     }
 
@@ -176,8 +174,8 @@ class Paciente{
         - Si está vacunada y no tiene síntomas, a los 5 días.
         - Si está vacunada y tiene síntomas, a los 7 días.*/
         if (this.validarEsquemaPrimario()){
-            var diaCero = new Date("12/01/2019")
-            var inicio = new Date(this.fis)
+            var diaCero = new Date("12/01/2019");
+            var inicio = new Date(this.fis);
             if (inicio.getTime() === diaCero.getTime())
                 dias = 5;
             else
@@ -188,14 +186,42 @@ class Paciente{
         fechaAlta.setDate(fechaAlta.getDate()+ dias)
         return fechaAlta;
     }
+
+    //Método que permite generar el HTML necesario para insertar pacientes
+    generarHTMLPaciente(){
+        //Generamos el texto para el esquema
+        let esquema = "";
+        if (this.validarEsquemaPrimario())
+            esquema = "COMPLETO";
+        else
+            esquema = "INCOMPLETO";
+
+        //Generamos el texto para las fechas
+        let fechaAlta = this.calcularFechaAlta().getDate() + "/" +
+                        (this.calcularFechaAlta().getMonth() + 1) + "/" + 
+                        this.calcularFechaAlta().getFullYear();
+        
+        let fechaCurva = this.calcularFechaDeCurva().getDate() + "/" +
+                        (this.calcularFechaDeCurva().getMonth() + 1) + "/" + 
+                        this.calcularFechaDeCurva().getFullYear();
+        
+        let html = '<h3>' + this.nombre + '</h3>\n' +
+                    '<div>Esquema ' + esquema + '</div>\n' +
+                    '<div>FC: ' + fechaCurva + '</div>\n' +
+                    '<div>Fecha estimada de alta: ' + fechaAlta + '</div>\n';
+                    
+        return html;
+    }
 }
 
+//Función para el agregado de pacientes
 const covid19CBA = () => {
     //Pedimos los datos del paciente    
     let nombre = prompt("Ingrese el nombre del paciente");
     let dni = prompt("Ingrese el DNI del paciente");
     var fis = new Date("12/01/2019");
     var ftm = new Date();
+    
     if (prompt("El paciente presenta síntomas.\nS: Sí\nOtro valor: no","S") === "S"){
         fis = Date.parse(prompt("Ingrese la fecha de inicio de síntomas en formato MM/DD/AAAA..."));
         while (isNaN(fis))
@@ -211,17 +237,46 @@ const covid19CBA = () => {
     //Instanciamos al paciente
     let paciente = new Paciente(dni, nombre, esquema, fis, ftm)
 
-    //Creamos el mensaje a mostrar
-    let mensaje = "";
-    if (paciente.calcularFechaAlta().getDate <= Date.now())
-        mensaje = "El paciente " + paciente.nombre + " DNI: " + paciente.dni + " ya está para el alta!";
-    else
-        mensaje = "El paciente " + paciente.nombre + " DNI: " + paciente.dni +
-                    " tiene fecha probable de alta el " +
-                    paciente.calcularFechaAlta().getDate() + "/" +
-                    (paciente.calcularFechaAlta().getMonth() + 1) + "/" + 
-                    paciente.calcularFechaAlta().getFullYear() + ".";
-    return mensaje;
+    //Lo sumamos a la sección de pacientes
+    let seccion = document.getElementById("section-pacientes");
+
+    let articulo = document.createElement('article');
+    articulo.className = "paciente";
+    articulo.innerHTML += paciente.generarHTMLPaciente();
+
+    seccion.appendChild(articulo);
 }
 
-alert(covid19CBA());
+//Función de eliminación de pacientes de la sección.
+const eliminarPacientes = () => {
+    let pacientes = document.getElementsByClassName("paciente");
+    let seccion = document.getElementById("section-pacientes");
+    if (pacientes.length > 0)
+        seccion.innerHTML = "";
+}
+
+const menu = () => {
+    let seccionMenu = document.getElementById("menu");
+
+    //Creamos un botón para llamar a la función covid19CBA que pide los datos del paciente.
+    let agregar = document.createElement('button');
+    agregar.className = "button-menu";
+    agregar.addEventListener('click', function(){
+        covid19CBA();
+    });
+    agregar.innerHTML = "Agregar paciente";
+
+    //Creamos un botón que llama a la función eliminarPacientes para eliminar los pacientes.
+    let eliminar = document.createElement('button');
+    eliminar.className = "button-menu";
+    eliminar.addEventListener('click', function(){
+        eliminarPacientes();
+    });
+    eliminar.innerHTML = "Limpiar pacientes";
+
+    //Con appendChild agregamos los botones a la sección del menu.
+    seccionMenu.appendChild(agregar);
+    seccionMenu.appendChild(eliminar);
+}
+
+menu();
